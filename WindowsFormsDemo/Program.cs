91517c7ca26fa0b7a47c5652b56936bfc8cd5f5c
Dave Greaves <davidmgreaves@gmail.com>
2017-09-07 17:38:41 +1000

    Add project files.

---
+++ WindowsFormsDemo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WindowsFormsDemo
+{
+	static class Program
+	{
+
+		// The main entry point for the application.
+		[STAThread]
+		static void Main()
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new Form1());
+		}
+	}
+}
